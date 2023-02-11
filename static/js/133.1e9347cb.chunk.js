8f2c2d20473f4e10da432b41fb5012126c1a2a5f
NastyaSavchenko <115043353+NastyaSavchenko@users.noreply.github.com>
2023-02-11 02:43:24 +0000

    Deploying to gh-pages from @ NastyaSavchenko/best-movies@31ff005095b69e0d3c09b2d3dc231d69412e4236 🚀

---
+++ static/js/133.1e9347cb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbest_movies=self.webpackChunkbest_movies||[]).push([[133],{3133:function(n,e,t){t.r(e),t.d(e,{default:function(){return h}});var i,s=t(9704),r=t(168),c=t(6444).ZP.main(i||(i=(0,r.Z)(["\n  min-height: calc(100vh - 64px);\n  padding: 20px;\n\n  > h1 {\n    margin-bottom: 20px;\n    text-align: center;\n  }\n"]))),a=t(184),h=function(){return(0,a.jsxs)(c,{children:[(0,a.jsx)("h1",{children:"Trending today"}),(0,a.jsx)(s.kE,{})]})}}}]);
+//# sourceMappingURL=133.1e9347cb.chunk.js.map
